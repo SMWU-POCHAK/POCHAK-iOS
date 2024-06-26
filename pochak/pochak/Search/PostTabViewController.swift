8cd9ebf574cb6ad851250b3c96aa9c3ba7ca0219
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2024-06-26 13:55:54 +0900

    Fix : 탐색탭 collectionview 디자인 수정

--- pochak/pochak/Search/PostTabViewController.swift
+++ pochak/pochak/Search/PostTabViewController.swift
@@ -34,6 +34,7 @@ class PostTabViewController: UIViewController, UISearchBarDelegate{
         
         // Clear search bar text and resign first responder when returning to this page
         self.searchBar.resignFirstResponder()
+        self.navigationController?.isNavigationBarHidden = true
     }
 
 
@@ -113,67 +114,31 @@ class PostTabViewController: UIViewController, UISearchBarDelegate{
 
 //MARK: - 포스트 collectionView
 extension PostTabViewController: UICollectionViewDelegate, UICollectionViewDataSource{
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 2
-    }
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        switch section {
-        case 0:
-            return 2
-        default:
-            return max(0,(postList.count)-2)
-        }
+        return postList.count
         
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let section = indexPath.section
-        switch section {
-        case 0:
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PostCollectionViewCell.identifier, for: indexPath) as? PostCollectionViewCell else{
-                fatalError("셀 타입 캐스팅 실패")
-            }
-            // 이미지 설정
-            if(!postList.isEmpty){
-                cell.configure(with: postList[indexPath.item].postImage)
-            }
-            return cell
-        default:
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PostCollectionViewCell.identifier, for: indexPath) as? PostCollectionViewCell else{
-                fatalError("셀 타입 캐스팅 실패2")
-            }
-            // 이미지 설정
-            if(!postList.isEmpty){
-                cell.configure(with: postList[indexPath.item+2].postImage)
-            }
-            
-            return cell
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PostCollectionViewCell.identifier, for: indexPath) as? PostCollectionViewCell else{
+            fatalError("셀 타입 캐스팅 실패2")
         }
+        // 이미지 설정
+        if(!postList.isEmpty){
+            cell.configure(with: postList[indexPath.item].postImage)
+        }
+        
+        return cell
     }
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let section = indexPath.section
-        switch section {
-        case 0:
-            print("view post btn tapped")
-            
-            let postTabSb = UIStoryboard(name: "PostTab", bundle: nil)
-            guard let postVC = postTabSb.instantiateViewController(withIdentifier: "PostVC") as? PostViewController
-                else { return }
-            
-            postVC.receivedPostId = postList[indexPath.item].postId
-            self.navigationController?.pushViewController(postVC, animated: true)
-            
-            
-        default:
-            print("view post btn tapped")
-            let postTabSb = UIStoryboard(name: "PostTab", bundle: nil)
-            guard let postVC = postTabSb.instantiateViewController(withIdentifier: "PostVC") as? PostViewController
-                else { return }
-            
-            postVC.receivedPostId = postList[indexPath.item+2].postId
-            self.navigationController?.pushViewController(postVC, animated: true)
-            
-        }
+        print("view post btn tapped")
+        let postTabSb = UIStoryboard(name: "PostTab", bundle: nil)
+        guard let postVC = postTabSb.instantiateViewController(withIdentifier: "PostVC") as? PostViewController
+            else { return }
+        
+        postVC.receivedPostId = postList[indexPath.item].postId
+        self.navigationController?.pushViewController(postVC, animated: true)
     }
 }
 
@@ -182,42 +147,27 @@ extension PostTabViewController: UICollectionViewDelegateFlowLayout {
     
     // 위 아래 간격
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        switch section {
-            
-        case 0:
-            return 0
-        default:
-            return 5
-        }
+        return 9
     }
 
         
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 2.5, left: 24, bottom: 2.5, right: 24)
+        return UIEdgeInsets(top: 4.5, left: 20, bottom: 4.5, right: 20)
         }
     // 옆 간격
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        switch section {
-            case 0:
-                return 6
-            default:
-                return 5
-            }
+        return 8
         }
 
             
     // cell 사이즈( 옆 라인을 고려하여 설정 )
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        let width = (collectionView.frame.width - 54) / 2 // 첫 번째 섹션의 셀 너비
+    
+        let width = (collectionView.frame.width-56) / 3 // 두 번째 섹션의 셀 너비
         let height = width * 4 / 3 // 셀의 가로:세로 비율
+        return CGSize(width: width, height: height) // 두 번째 섹션의 셀 크기
         
-        if indexPath.section == 0 {
-            return CGSize(width: width, height: height) // 첫 번째 섹션의 셀 크기
-        } else {
-            let secWidth = (collectionView.frame.width - 58) / 3 // 두 번째 섹션의 셀 너비
-            let secHeight = secWidth * 4 / 3 // 셀의 가로:세로 비율
-            return CGSize(width: secWidth, height: secHeight) // 두 번째 섹션의 셀 크기
-        }
+       
     }
 }
+
