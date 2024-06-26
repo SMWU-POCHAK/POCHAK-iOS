5cb7216335d8eb67f33c7cdf6163af54350d9ebd
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2024-06-26 16:49:11 +0900

    Fix : UISearchBar, UIImage 익스텐션 분리

--- pochak/pochak/Extensions/UISearchBar+.swift
+++ pochak/pochak/Extensions/UISearchBar+.swift
@@ -133,15 +133,3 @@ extension UISearchBar {
     }
     
 }
-
-extension UIImage {
-    convenience init(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) {
-        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
-        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0)
-        color.setFill()
-        UIRectFill(rect)
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        self.init(cgImage: (image?.cgImage!)!)
-    }
-}
