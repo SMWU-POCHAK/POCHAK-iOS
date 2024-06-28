bb1292a29c986d7b1f34cb520b415abae7faadb7
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2024-06-29 04:02:45 +0900

    Feat : 최근검색어탭 뷰 구현

--- pochak/pochak/Search/RecentSearchViewController.swift
+++ pochak/pochak/Search/RecentSearchViewController.swift
@@ -12,36 +12,73 @@ import RealmSwift
 
 class RecentSearchViewController: UIViewController, UITextFieldDelegate {
     
+    // MARK: - Views
+    
     @IBOutlet weak var deleteAllButton: UILabel!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchContainerView: UIView!
     
+    // MARK: - Properties
+    
     var searchTextField = UITextField()
     var cancelButton = UIButton()
-    var isFiltering: Bool = false
     
     var resultVC = UITableViewController()
 
     let realm = try! Realm()
     var realmManager = RecentSearchRealmManager()
     var recentSearchTerms: Results<RecentSearchModel>!
-    var filteredSearchTerms: Results<RecentSearchModel>!
     var searchResultData : [idSearchResponse] = []
 
     var searchTextFieldWidthConstraint: NSLayoutConstraint!
     var cancelButtonLeadingConstraint: NSLayoutConstraint!
 
-        
+    // MARK: - lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
         print("==RecentSearchViewController==")
-        self.navigationController?.isNavigationBarHidden = true
-        setupTableView()
+        
+        // back 버튼 커스텀
+        self.navigationItem.backButtonTitle = ""
+        self.navigationController?.navigationBar.tintColor = .black
+        
+        deleteAllBtnAction()
         setupSearchTextField()
-        loadRealm()
         setupResultViewController()
+        setupResultViewLayout()
+        setupTableView()
+        loadRealm()
+
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.isNavigationBarHidden = true
+        setupResultViewLayout()
+    }
+    // MARK: - Functions
+    
+    // 전체 삭제 액션
+    private func deleteAllBtnAction(){
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(labelTapped))
+        deleteAllButton.isUserInteractionEnabled = true
+        deleteAllButton.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func labelTapped() {
+        print("deleteAll")
+        if realmManager.deleteAllData() {
+            // 데이터 삭제에 성공한 경우
+            recentSearchTerms = realmManager.getAllRecentSearchTerms()
+            tableView.reloadData()
+        } else {
+            // 데이터 삭제에 실패한 경우
+            print("Failed to delete all data")
+        }
+    }
+    
+    // 검색바 설정
     private func setupSearchTextField() {
         // 검색 textfield 설정
         searchTextField.delegate = self
@@ -100,52 +137,9 @@ class RecentSearchViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
-    private func setupResultViewController() {
-        // 검색 결과 tableViewController 설정
-        resultVC = UITableViewController()
-        resultVC.tableView.delegate = self
-        resultVC.tableView.dataSource = self
-        resultVC.tableView.separatorStyle = .none
-        resultVC.tableView.register(UINib(nibName: "SearchResultTableViewCell", bundle: nil), forCellReuseIdentifier: "SearchResultTableViewCell")
-        
-        resultVC.view.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(resultVC.view)
-        
-        NSLayoutConstraint.activate([
-            resultVC.view.topAnchor.constraint(equalTo: searchContainerView.bottomAnchor),
-            resultVC.view.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            resultVC.view.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            resultVC.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ])
-        
-        resultVC.view.isHidden = true // 초기에는 숨겨진 상태로 설정
-    }
-   
-    private func setupTableView(){
-        tableView.delegate = self
-        tableView.dataSource = self
-
-        tableView.separatorStyle = .none
-        tableView.register(UINib(nibName: "SearchResultTableViewCell", bundle: nil), forCellReuseIdentifier: "SearchResultTableViewCell")
-    }
-
-    func loadRealm(){
-        recentSearchTerms = realmManager.getAllRecentSearchTerms()
-        print(recentSearchTerms)
-        tableView.reloadData()
-    }
-    
-    @objc private func cancelButtonClicked() {
-        searchTextField.text = ""
-        searchTextField.resignFirstResponder()
-        isFiltering = false
-        loadRealm()
-    }
-    
-    // 검색어 입력 시작
+    // 검색바 입력 시작
     func textFieldDidBeginEditing(_ textField: UITextField) {
         cancelButton.isHidden = false
-        resultVC.tableView.isHidden = false
 
         UIView.animate(withDuration: 0.3) {
             self.searchTextFieldWidthConstraint.constant = self.searchContainerView.frame.width - 41
@@ -160,13 +154,12 @@ class RecentSearchViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
-    // 검색어 입력 끝
+    // 검색바 입력 끝
     func textFieldDidEndEditing(_ textField: UITextField) {
         UIView.animate(withDuration: 0.3, animations: {
             self.searchTextFieldWidthConstraint.constant = self.searchContainerView.frame.width
             self.cancelButtonLeadingConstraint.constant = 0
             
-            self.resultVC.tableView.isHidden = true
             self.resultVC.tableView.frame = CGRect(x: self.searchContainerView.frame.origin.x,
                                                    y: self.searchContainerView.frame.maxY,
                                                    width: self.searchContainerView.frame.width,
@@ -177,14 +170,16 @@ class RecentSearchViewController: UIViewController, UITextFieldDelegate {
         }
     }
 
+    // 검색바 텍스트
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         let currentText = (textField.text as NSString?)?.replacingCharacters(in: range, with: string) ?? string
         if currentText.isEmpty {
-            isFiltering = false
-            loadRealm()
+            self.resultVC.tableView.isHidden = true
+            self.searchResultData = []
+            self.resultVC.tableView.reloadData()
         } else {
-            isFiltering = true
-            filteredSearchTerms = recentSearchTerms.filter("term CONTAINS[c] %@", currentText)
+            self.resultVC.tableView.isHidden = false
+            sendTextToServer(currentText)
             tableView.reloadData()
         }
         return true
@@ -195,32 +190,51 @@ class RecentSearchViewController: UIViewController, UITextFieldDelegate {
         return true
     }
     
-    @objc func labelTapped() {
-        print("deleteAll")
-        if realmManager.deleteAllData() {
-            // 데이터 삭제에 성공한 경우
-            recentSearchTerms = realmManager.getAllRecentSearchTerms()
-            tableView.reloadData()
-        } else {
-            // 데이터 삭제에 실패한 경우
-            print("Failed to delete all data")
-        }
+    // 취소 버튼 클릭
+    @objc private func cancelButtonClicked() {
+        searchTextField.text = ""
+        searchTextField.resignFirstResponder()
+        resultVC.view.isHidden = true
+        loadRealm()
     }
     
-    func updateSearchResults(for searchController: UISearchController) {
-        guard let text = searchController.searchBar.text else  {
-            return
-        }
-        print(text)
-        if(text != ""){
-            sendTextToServer(text)
-        }
-        else{
-            print("===text null ===")
-            print(text)
-            self.searchResultData = []
-            self.resultVC.tableView.reloadData()
-        }
+    // 검색 결과
+    private func setupResultViewController() {
+        // 검색 결과 tableViewController 설정
+        resultVC = UITableViewController()
+        resultVC.tableView.delegate = self
+        resultVC.tableView.dataSource = self
+        resultVC.tableView.separatorStyle = .none
+        resultVC.tableView.register(UINib(nibName: "SearchResultTableViewCell", bundle: nil), forCellReuseIdentifier: "SearchResultTableViewCell")
+        
+        resultVC.view.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(resultVC.view)
+        
+        resultVC.view.isHidden = true // 초기에는 숨겨진 상태로 설정
+    }
+   
+    // 검색 결과 테이블 뷰 레이아웃 설정
+    private func setupResultViewLayout(){
+        NSLayoutConstraint.activate([
+            resultVC.view.topAnchor.constraint(equalTo: searchContainerView.bottomAnchor, constant: 16),
+            resultVC.view.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            resultVC.view.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            resultVC.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    private func setupTableView(){
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        tableView.separatorStyle = .none
+        tableView.register(UINib(nibName: "SearchResultTableViewCell", bundle: nil), forCellReuseIdentifier: "SearchResultTableViewCell")
+    }
+
+    func loadRealm(){
+        recentSearchTerms = realmManager.getAllRecentSearchTerms()
+        print(recentSearchTerms)
+        tableView.reloadData()
     }
     
     func sendTextToServer(_ searchText: String) {
@@ -262,6 +276,7 @@ extension RecentSearchViewController:UITableViewDelegate, UITableViewDataSource 
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SearchResultTableViewCell", for: indexPath) as! SearchResultTableViewCell
+        
         if tableView == self.tableView {
             let recentSearchTerm = recentSearchTerms[indexPath.row]
             print(recentSearchTerm)
@@ -295,38 +310,35 @@ extension RecentSearchViewController:UITableViewDelegate, UITableViewDataSource 
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if tableView == self.tableView {
-            let selectedUserData = recentSearchTerms[indexPath.row] // 선택한 셀의 데이터 가져오기
+            let selectedUserData = recentSearchTerms[indexPath.row]
             let handle = selectedUserData.term
             let name = selectedUserData.name
             let profileImg = selectedUserData.profileImg
             
-            realmManager.addRecentSearch(term: handle, profileImg: profileImg, name: name)
-            self.tableView.reloadData()
-            // 화면전환
-            // TODO: handle 전달
-            let storyboard = UIStoryboard(name: "ProfileTab", bundle: nil)
-            let profileTabVC = storyboard.instantiateViewController(withIdentifier: "OtherUserProfileVC") as! OtherUserProfileViewController
-            
-            profileTabVC.recievedHandle = handle
-            self.navigationController?.pushViewController(profileTabVC, animated: true)
+            handleSelectedUser(handle: handle, name: name, profileImg: profileImg)
+            tableView.deselectRow(at: indexPath, animated: true)
             
         } else if tableView == self.resultVC.tableView {
-            let selectedUserData = searchResultData[indexPath.row] // 선택한 셀의 데이터 가져오기
+            let selectedUserData = searchResultData[indexPath.row]
             let handle = selectedUserData.handle
             let name = selectedUserData.name
             let profileImg = selectedUserData.profileImage
-
-            realmManager.addRecentSearch(term: handle, profileImg: profileImg, name: name)
-            self.tableView.reloadData()
-
-            // 화면전환 -> handle 전달
-            // TODO: handle 전달
-            let storyboard = UIStoryboard(name: "ProfileTab", bundle: nil)
-            let profileTabVC = storyboard.instantiateViewController(withIdentifier: "OtherUserProfileVC") as! OtherUserProfileViewController
             
-            profileTabVC.recievedHandle = handle
-            self.navigationController?.pushViewController(profileTabVC, animated: true)
-            
+            handleSelectedUser(handle: handle, name: name, profileImg: profileImg)
+            tableView.deselectRow(at: indexPath, animated: true)
         }
     }
+    
+    func handleSelectedUser(handle: String, name: String, profileImg: String) {
+        // 최근 검색어 추가
+        realmManager.addRecentSearch(term: handle, profileImg: profileImg, name: name)
+        loadRealm()
+        // 화면 전환
+        let storyboard = UIStoryboard(name: "ProfileTab", bundle: nil)
+        let profileTabVC = storyboard.instantiateViewController(withIdentifier: "OtherUserProfileVC") as! OtherUserProfileViewController
+        
+        profileTabVC.recievedHandle = handle
+        self.navigationController?.pushViewController(profileTabVC, animated: true)
+    }
+
 }
