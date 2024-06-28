08b98e9105062ee60b95ab3e85c0e91130ed2f6e
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2024-06-28 21:59:14 +0900

    Feat : 최근검색어탭 검색바 구현

--- pochak/pochak/Search/RecentSearchViewController.swift
+++ pochak/pochak/Search/RecentSearchViewController.swift
@@ -10,88 +10,118 @@ import Foundation
 import UIKit
 import RealmSwift
 
-class RecentSearchViewController: UIViewController, UISearchResultsUpdating {
+class RecentSearchViewController: UIViewController, UITextFieldDelegate {
     
+    @IBOutlet weak var deleteAllButton: UILabel!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchContainerView: UIView!
     
+    var searchTextField = UITextField()
+    var cancelButton = UIButton()
     var isFiltering: Bool = false
-
-    var searchController = UISearchController()
-
+    
     var resultVC = UITableViewController()
 
     let realm = try! Realm()
-    
-    @IBOutlet weak var deleteAllButton: UILabel!
-    
     var realmManager = RecentSearchRealmManager()
     var recentSearchTerms: Results<RecentSearchModel>!
-    
+    var filteredSearchTerms: Results<RecentSearchModel>!
     var searchResultData : [idSearchResponse] = []
-    
+
+    var searchTextFieldWidthConstraint: NSLayoutConstraint!
+    var cancelButtonLeadingConstraint: NSLayoutConstraint!
+
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         print("==RecentSearchViewController==")
-        self.navigationController?.isNavigationBarHidden = false
-        self.navigationItem.hidesBackButton = true
+        self.navigationController?.isNavigationBarHidden = true
         setupTableView()
-        setUpSearchController()
-        
+        setupSearchTextField()
         loadRealm()
+        setupResultViewController()
     }
     
-    private func setUpSearchController() {
-        // Create an instance of the view controller that will display search results
-    
+    private func setupSearchTextField() {
+        // 검색 textfield 설정
+        searchTextField.delegate = self
+        searchTextField.placeholder = "검색어를 입력해주세요."
+        searchTextField.font = UIFont(name: "Pretendard-Medium", size: 16)
+        searchTextField.backgroundColor = UIColor(named: "gray0.5")
+        searchTextField.layer.cornerRadius = 7
+        searchTextField.translatesAutoresizingMaskIntoConstraints = false
+        searchTextField.clearButtonMode = .never
+        searchTextField.returnKeyType = .search
+        searchTextField.setPlaceholderColor(UIColor(named: "gray03"), font: "Pretendard-Medium", fontSize: 16)
+        searchTextField.tintColor = .black
         
-        searchController = UISearchController(searchResultsController: resultVC)
-        searchController.searchResultsUpdater  = self
-        searchController.obscuresBackgroundDuringPresentation = false
-        searchController.searchBar.searchBarStyle = UISearchBar.Style.prominent
+        // 검색 아이콘 설정
+        let iconView = UIImageView(frame: CGRect(x: 12, y: 12, width: 24, height: 24)) // set your Own size
+        iconView.image = UIImage(named: "search")
+        let iconContainerView: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 48, height: 48))
+        iconContainerView.addSubview(iconView)
+        searchTextField.leftView = iconContainerView
         
-        searchController.searchBar.setBackgroundColor(UIColor(named: "gray0.5"))
+        searchTextField.leftViewMode = .always
+        
+        // 취소 버튼 설정
+        cancelButton.setTitle("취소", for: .normal)
+        cancelButton.titleLabel?.font = UIFont(name: "Pretendard-Bold", size: 14)
+        cancelButton.setTitleColor(.black, for: .normal)
+        cancelButton.addTarget(self, action: #selector(cancelButtonClicked), for: .touchUpInside)
+        cancelButton.translatesAutoresizingMaskIntoConstraints = false
+        cancelButton.isHidden = true
+        
+        // 뷰 설정
+        if let searchContainerView = searchContainerView {
+            searchContainerView.addSubview(searchTextField)
+            searchContainerView.addSubview(cancelButton)
+            
+            // searchTextField
+            NSLayoutConstraint.activate([
+                searchTextField.leadingAnchor.constraint(equalTo: searchContainerView.leadingAnchor),
+                searchTextField.topAnchor.constraint(equalTo: searchContainerView.topAnchor),
+                searchTextField.bottomAnchor.constraint(equalTo: searchContainerView.bottomAnchor)
+            ])
+            
 
-        searchController.searchBar.setLeftImage(UIImage(named: "search"), tintColor: UIColor(named: "gray06"))
-        
-        if let textFieldInsideSearchBar = searchController.searchBar.value(forKey: "searchField") as? UITextField {
-            textFieldInsideSearchBar.textColor = UIColor.black
-            textFieldInsideSearchBar.font = UIFont(name: "Pretendard-Medium", size: 16)
-            textFieldInsideSearchBar.attributedPlaceholder = NSAttributedString(
-                string: "검색어를 입력해주세요.", // 원하는 placeholder 텍스트
-                attributes: [NSAttributedString.Key.foregroundColor: UIColor(named: "gray03"),
-                             NSAttributedString.Key.font: UIFont(name: "Pretendard-Medium", size: 16) ] // 원하는 색으로 변경
-                
-            )
+            searchTextFieldWidthConstraint = searchTextField.widthAnchor.constraint(equalToConstant: self.searchContainerView.frame.width)
+            searchTextFieldWidthConstraint.isActive = true
+            
+            // cancelButton
+            NSLayoutConstraint.activate([
+                cancelButton.leadingAnchor.constraint(equalTo: searchTextField.trailingAnchor, constant: 10),
+                cancelButton.trailingAnchor.constraint(equalTo: searchContainerView.trailingAnchor),
+                cancelButton.centerYAnchor.constraint(equalTo: searchTextField.centerYAnchor)
+            ])
+            
+            cancelButtonLeadingConstraint = cancelButton.leadingAnchor.constraint(equalTo: searchTextField.trailingAnchor)
+            cancelButtonLeadingConstraint.isActive = true
         }
-        searchController.searchBar.searchTextPositionAdjustment = UIOffset(horizontal: 10, vertical: 0)
-        
-        
-        // Change Cancel button value
-        searchController.searchBar.setValue("취소", forKey: "cancelButtonText")
-        searchController.searchBar.autocapitalizationType = .none
-        //usally good to set the presentation context
-        
-        self.searchController.hidesNavigationBarDuringPresentation = false
-        self.searchController.searchBar.searchBarStyle = UISearchBar.Style.minimal
-
-        // Include the search bar within the navigation bar.
-        self.navigationItem.titleView = self.searchController.searchBar
-        self.definesPresentationContext = true
-        
-        searchController.searchBar.sizeToFit()
-        
-        // 검색 결과 tableview 세팅
-        let horizontalSpacing: CGFloat = 24.0
-        resultVC.tableView.contentInset = UIEdgeInsets(top: 0, left: horizontalSpacing, bottom: 0, right: horizontalSpacing)
-
+    }
+    
+    private func setupResultViewController() {
+        // 검색 결과 tableViewController 설정
+        resultVC = UITableViewController()
         resultVC.tableView.delegate = self
         resultVC.tableView.dataSource = self
         resultVC.tableView.separatorStyle = .none
         resultVC.tableView.register(UINib(nibName: "SearchResultTableViewCell", bundle: nil), forCellReuseIdentifier: "SearchResultTableViewCell")
+        
+        resultVC.view.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(resultVC.view)
+        
+        NSLayoutConstraint.activate([
+            resultVC.view.topAnchor.constraint(equalTo: searchContainerView.bottomAnchor),
+            resultVC.view.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            resultVC.view.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            resultVC.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        resultVC.view.isHidden = true // 초기에는 숨겨진 상태로 설정
     }
-    
+   
     private func setupTableView(){
-        //delegate 연결
         tableView.delegate = self
         tableView.dataSource = self
 
@@ -104,7 +134,67 @@ class RecentSearchViewController: UIViewController, UISearchResultsUpdating {
         print(recentSearchTerms)
         tableView.reloadData()
     }
+    
+    @objc private func cancelButtonClicked() {
+        searchTextField.text = ""
+        searchTextField.resignFirstResponder()
+        isFiltering = false
+        loadRealm()
+    }
+    
+    // 검색어 입력 시작
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        cancelButton.isHidden = false
+        resultVC.tableView.isHidden = false
 
+        UIView.animate(withDuration: 0.3) {
+            self.searchTextFieldWidthConstraint.constant = self.searchContainerView.frame.width - 41
+            self.cancelButtonLeadingConstraint.constant = 16
+            self.view.layoutIfNeeded()
+            
+            self.resultVC.tableView.frame = CGRect(x: self.searchContainerView.frame.origin.x,
+                                                   y: self.searchContainerView.frame.maxY,
+                                                   width: self.searchContainerView.frame.width,
+                                                   height: self.view.frame.height - self.searchContainerView.frame.maxY)
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    // 검색어 입력 끝
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.searchTextFieldWidthConstraint.constant = self.searchContainerView.frame.width
+            self.cancelButtonLeadingConstraint.constant = 0
+            
+            self.resultVC.tableView.isHidden = true
+            self.resultVC.tableView.frame = CGRect(x: self.searchContainerView.frame.origin.x,
+                                                   y: self.searchContainerView.frame.maxY,
+                                                   width: self.searchContainerView.frame.width,
+                                                   height: self.view.frame.height - self.searchContainerView.frame.maxY)
+            self.view.layoutIfNeeded()
+        }) { _ in
+            self.cancelButton.isHidden = true
+        }
+    }
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let currentText = (textField.text as NSString?)?.replacingCharacters(in: range, with: string) ?? string
+        if currentText.isEmpty {
+            isFiltering = false
+            loadRealm()
+        } else {
+            isFiltering = true
+            filteredSearchTerms = recentSearchTerms.filter("term CONTAINS[c] %@", currentText)
+            tableView.reloadData()
+        }
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
     @objc func labelTapped() {
         print("deleteAll")
         if realmManager.deleteAllData() {
