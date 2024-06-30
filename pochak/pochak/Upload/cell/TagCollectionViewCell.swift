9a30756677bff167d5d5e4cb8f183090157a6f30
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2024-06-30 20:54:56 +0900

    Feat : 업로드탭 태그 관련 셀 ui

--- pochak/pochak/Upload/cell/TagCollectionViewCell.swift
+++ pochak/pochak/Upload/cell/TagCollectionViewCell.swift
@@ -9,37 +9,30 @@ import UIKit
 
 class TagCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var deleteBtn: UIImageView!
     @IBOutlet weak var tagIdLabel: UILabel!
-    @IBOutlet weak var labelBg: UIView!
+    
     static let identifier = "TagCollectionViewCell"
+    
+    var deleteButtonAction: (() -> Void)?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-        
-        labelBg.layer.cornerRadius = 8
-        labelBg.layer.borderWidth = 1
-        labelBg.layer.borderColor = UIColor(hexCode: "79747E").cgColor
+        setupViewCornerRadius()
+        setupDeleteButton()
+    }
+    
+    private func setupViewCornerRadius(){
+        self.layer.cornerRadius = 6
     }
 
-}
-
-extension UIColor {
+    private func setupDeleteButton() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(deleteButtonTapped))
+        deleteBtn.isUserInteractionEnabled = true
+        deleteBtn.addGestureRecognizer(tapGestureRecognizer)
+    }
     
-    convenience init(hexCode: String, alpha: CGFloat = 1.0) {
-        var hexFormatted: String = hexCode.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
-        
-        if hexFormatted.hasPrefix("#") {
-            hexFormatted = String(hexFormatted.dropFirst())
-        }
-        
-        assert(hexFormatted.count == 6, "Invalid hex code used.")
-        
-        var rgbValue: UInt64 = 0
-        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
-        
-        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-                  alpha: alpha)
+    @objc private func deleteButtonTapped() {
+        deleteButtonAction?()
     }
 }
