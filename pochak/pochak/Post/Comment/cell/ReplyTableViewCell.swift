941880be22278a6fd27d5c5cd5437a13601133f1
Suyeon <syhhwang1231@gmail.com>
2024-07-01 03:13:43 +0900

    Feat: 이미지 로딩 시 extension 함수 사용

--- pochak/pochak/Post/Comment/cell/ReplyTableViewCell.swift
+++ pochak/pochak/Post/Comment/cell/ReplyTableViewCell.swift
@@ -77,20 +77,9 @@ class ReplyTableViewCell: UITableViewCell {
         parentCommentId = commentData.parentId
         
         // 프로필 이미지
-        let profileImgStr = commentData.profileImage
-            let url = URL(string: profileImgStr)
-            // main thread에서 load할 경우 URL 로딩이 길면 화면이 멈춘다.
-            // 이를 방지하기 위해 다른 thread에서 처리함.
-            DispatchQueue.global().async { [weak self] in
-                if let data = try? Data(contentsOf: url!) {
-                    if let image = UIImage(data: data) {
-                        //UI 변경 작업은 main thread에서 해야함.
-                        DispatchQueue.main.async {
-                            self?.profileImageView.image = image
-                        }
-                    }
-                }
-            }
+        if let url = URL(string: commentData.profileImage) {
+            profileImageView.load(url: url)
+        }
         
         // 유저 핸들
         userHandleLabel.text = commentData.handle
