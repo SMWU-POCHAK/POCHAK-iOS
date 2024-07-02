6f93524ebf51e43799578c37e1a86766bdf30dd9
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2024-07-02 14:51:45 +0900

    Fix : 카메라탭 에러 해결

--- pochak/pochak/Upload/CameraViewController.swift
+++ pochak/pochak/Upload/CameraViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 import AVFoundation
 import SwiftUI
 
-
 class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate {
     
     @IBOutlet weak var previewView: UIView!
@@ -35,28 +34,20 @@ class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate {
     @Published var isFlashOn = false
     
     @IBOutlet weak var flashbtn: UIButton!
-    @IBAction func flashBtn(_ sender: Any) { //flash 버튼 클릭시
+    
+    @IBAction func flashBtn(_ sender: Any) {
         switchFlash()
         print("flash")
     }
-    @IBAction func captureBtn(_ sender: Any) { //카메라 버튼 클릭시
+    
+    @IBAction func captureBtn(_ sender: Any) {
         if isCameraBusy == false {
             hapticImpact.impactOccurred()
-            withAnimation(.easeInOut(duration: 0.1)) {
-                shutterEffect = true
-            }
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-                withAnimation(.easeInOut(duration: 0.1)) {
-                    self.shutterEffect = false
-                }
-            }
             
             let photoSettings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])
             photoSettings.flashMode = self.flashMode
             
-            
             stillImageOutput.capturePhoto(with: photoSettings, delegate: self)
-            
             
             print("[Camera]: Photo's taken")
             print("[CameraViewModel]: Photo captured!")
@@ -64,20 +55,17 @@ class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate {
             print("[CameraViewModel]: Camera's busy.")
         }
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "Pretendard-bold", size: 20)!]
         self.navigationItem.title = "포착하기"
-        
- 
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        // Setup your camera here...
         requestAndCheckPermissions()
-        
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -86,27 +74,18 @@ class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate {
     }
     
     func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
-        
         guard let imageData = photo.fileDataRepresentation(), let image = UIImage(data: imageData) else {
-                return
-            }
+            return
+        }
         
-////         이미지 저장 코드 (옵션)
-//        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
-//        print("[Camera]: Photo saved")
-        
-        // 이미지 전달 및 페이지 전환
         let uploadViewController = storyboard?.instantiateViewController(withIdentifier: "UploadViewController") as! UploadViewController
         uploadViewController.receivedImage = image
         navigationController?.pushViewController(uploadViewController, animated: true)
-
     }
     
     func requestAndCheckPermissions() {
-        // 카메라 권한 상태 확인
         switch AVCaptureDevice.authorizationStatus(for: .video) {
         case .notDetermined:
-            // 권한 요청
             AVCaptureDevice.requestAccess(for: .video) { [weak self] authStatus in
                 if authStatus {
                     DispatchQueue.main.async {
@@ -117,72 +96,69 @@ class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate {
         case .restricted:
             break
         case .authorized:
-            // 이미 권한 받은 경우 셋업
             setUpCamera()
         default:
-            // 거절했을 경우
-            print("Permession declined")
+            print("Permission declined")
         }
     }
-    // 플래시 온오프
+    
     func switchFlash() {
         isFlashOn.toggle()
         flashMode = isFlashOn == true ? .on : .off
         
-        // 이미지 설정
         let buttonImageName = isFlashOn ? "flashActiveBg" : "flashBg"
         flashBtnBg.setImage(UIImage(named: buttonImageName), for: .normal)
-        
     }
-    //카메라 세팅
-    func setUpCamera(){
+    
+    func setUpCamera() {
+        DispatchQueue.global(qos: .userInitiated).async {
+            self.captureSession = AVCaptureSession()
 
-        captureSession = AVCaptureSession()
-        captureSession.startRunning()
-        
-        guard let backCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) else { return }
-        
-        do {
-            let input = try AVCaptureDeviceInput(device: backCamera)
-            
-            let photoSettings = AVCapturePhotoSettings()
-            photoSettings.isHighResolutionPhotoEnabled = false // 고해상도 비활성화
-            
-            if backCamera.supportsSessionPreset(.photo) {
-                captureSession.sessionPreset = .photo
+            guard let backCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) else {
+                return
             }
-            
-            stillImageOutput = AVCapturePhotoOutput()
-            
-            if captureSession.canAddInput(input) && captureSession.canAddOutput(stillImageOutput) {
-                captureSession.addInput(input)
-                captureSession.addOutput(stillImageOutput)
-                setupLivePreview()
-            }
-        }
-        catch let error  {
-            print("Error Unable to initialize back camera:  \(error.localizedDescription)")
-        }
-        
-        func setupLivePreview() {
-            
-            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-            videoPreviewLayer.videoGravity = .resizeAspectFill
-            
-            videoPreviewLayer.connection?.videoOrientation = .portrait
-            // 비디오 출력의 해상도를 4:3 비율로 설정
-            let previewSize = CGSize(width: 640, height: 480) // 4:3 비율로 원하는 해상도 설정
-            videoPreviewLayer.frame = CGRect(origin: .zero, size: previewSize)
-            videoPreviewLayer.position = CGPoint(x: previewView.bounds.midX, y: previewView.bounds.midY)
-            
-            previewView.layer.addSublayer(videoPreviewLayer)
-            
-            DispatchQueue.global(qos: .userInitiated).async { //[weak self] in
-                self.captureSession.startRunning()
-                DispatchQueue.main.async {
-                    self.videoPreviewLayer.frame = self.previewView.bounds
+
+            do {
+                let input = try AVCaptureDeviceInput(device: backCamera)
+
+                let photoSettings = AVCapturePhotoSettings()
+                photoSettings.isHighResolutionPhotoEnabled = false
+
+                if backCamera.supportsSessionPreset(.photo) {
+                    self.captureSession.sessionPreset = .photo
                 }
+
+                self.stillImageOutput = AVCapturePhotoOutput()
+
+                if self.captureSession.canAddInput(input) && self.captureSession.canAddOutput(self.stillImageOutput) {
+                    self.captureSession.addInput(input)
+                    self.captureSession.addOutput(self.stillImageOutput)
+
+                    // 라이브 프리뷰 설정을 메인 스레드에서 호출
+                    DispatchQueue.main.async {
+                        self.setupLivePreview()
+                    }
+
+                    // captureSession.startRunning()을 백그라운드 스레드에서 호출
+                    self.captureSession.startRunning()
+                }
+            } catch let error  {
+                print("Error Unable to initialize back camera:  \(error.localizedDescription)")
             }
+        }
+    }
+
+    func setupLivePreview() {
+        DispatchQueue.main.async { // 메인 스레드에서 실행되어야 함
+            self.videoPreviewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
+            self.videoPreviewLayer.videoGravity = .resizeAspectFill
+            self.videoPreviewLayer.connection?.videoOrientation = .portrait
+            
+            // previewView의 bounds에 맞게 frame 설정
+            self.videoPreviewLayer.frame = self.previewView.bounds
+            
+            // previewView의 서브레이어로 추가
+            self.previewView.layer.addSublayer(self.videoPreviewLayer)
         }
     }
 }
