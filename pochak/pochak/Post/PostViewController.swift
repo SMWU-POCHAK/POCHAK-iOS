6b9425861fe35aa438a11ff27374dca37378f37c
Suyeon <syhhwang1231@gmail.com>
2024-07-06 03:37:06 +0900

    Feat: 게시글 상세 > 팔로우 취소 시 알림창 띄우기

--- pochak/pochak/Post/PostViewController.swift
+++ pochak/pochak/Post/PostViewController.swift
@@ -35,6 +35,7 @@ class PostViewController: UIViewController, UISheetPresentationControllerDelegat
     
     private var isFollowingColor: UIColor = UIColor(named: "gray03") ?? UIColor(hexCode: "FFB83A")
     private var isNotFollowingColor: UIColor = UIColor(named: "yellow00") ?? UIColor(hexCode: "C6CDD2")
+    private var isFollowing: Bool?
     
     private var postDataResponse: PostDataResponse!
     private var postDataResult: PostDataResponseResult!
@@ -141,7 +142,6 @@ class PostViewController: UIViewController, UISheetPresentationControllerDelegat
             }
         }
         
-        // TODO: 태그된 사용자에 프로필 이동 제스쳐 등록하기
         self.pochakUserLabel.text = postDataResult.ownerHandle + "님이 포착"
         
         // 포스트 내용
@@ -162,13 +162,13 @@ class PostViewController: UIViewController, UISheetPresentationControllerDelegat
         self.likeButton.isSelected = postDataResult.isLike
         
         // 팔로잉 버튼
-        if(self.postDataResult.isFollow == nil){
+        if(isFollowing == nil){
             self.followingBtn.isHidden = true
         }
         else{
             self.followingBtn.isHidden = false
-            self.followingBtn.isSelected = postDataResult.isFollow!
-            self.followingBtn.backgroundColor = postDataResult.isFollow! ? self.isFollowingColor : self.isNotFollowingColor
+            self.followingBtn.isSelected = isFollowing!
+            self.followingBtn.backgroundColor = isFollowing! ? self.isFollowingColor : self.isNotFollowingColor
         }
     }
     
@@ -181,6 +181,7 @@ class PostViewController: UIViewController, UISheetPresentationControllerDelegat
                 self.postDataResult = self.postDataResponse.result
                 self.postOwnerHandle = self.postDataResult.ownerHandle
                 self.taggedUserList = self.postDataResult.tagList
+                self.isFollowing = self.postDataResult.isFollow
                 self.initUI()
             case .requestErr(let message):
                 print("requestErr", message)
@@ -240,10 +241,8 @@ class PostViewController: UIViewController, UISheetPresentationControllerDelegat
         commentUserHandleLabel.text = postDataResult.recentComment?.handle
         commentContentLabel.text = postDataResult.recentComment?.content
     }
-       
-    // MARK: - Actions
     
-    @IBAction func followingBtnTapped(_ sender: Any) {
+    private func postFollowRequest() {
         FollowDataService.shared.postFollow(postOwnerHandle) { response in
             switch(response) {
             case .success(let followData):
@@ -254,9 +253,6 @@ class PostViewController: UIViewController, UISheetPresentationControllerDelegat
                 }
                 // 바뀐 데이터 반영 위해 다시 포스트 상세 데이터 로드
                 self.loadPostDetailData()
-                
-                // 팔로잉 혹은 취소하면 홈탭에서 보여주는게 달라져야함
-//                Home.UserFollowChanged = true
             case .requestErr(let message):
                 print("requestErr", message)
             case .pathErr:
@@ -268,6 +264,23 @@ class PostViewController: UIViewController, UISheetPresentationControllerDelegat
                 print("networkFail")
                 self.present(UIAlertController.networkErrorAlert(title: "네트워크 연결에 문제가 있습니다."), animated: true)
             }
+        }
+    }
+       
+    // MARK: - Actions
+    
+    @IBAction func followingBtnTapped(_ sender: Any) {
+        print("현재 팔로잉 상태: \(isFollowing)")
+        
+        // 현재 팔로잉 상태라면 취소할건지 알림창 띄우기
+        if isFollowing! {
+            showAlert(alertType: .confirmAndCancel,
+                      titleText: "팔로우를 취소할까요?",
+                      cancelButtonText: "취소",
+                      confirmButtonText: "확인")
+        }
+        else {
+            postFollowRequest()
         }
     }
 
@@ -431,4 +444,16 @@ extension UIScrollView {
 
 extension PostViewController: UIGestureRecognizerDelegate {
 
+}
+
+// MARK: - Extension; Custom Alert delegate
+
+extension PostViewController: CustomAlertDelegate {
+    func confirmAction() {
+        postFollowRequest()
+    }
+    
+    func cancel() {
+        print("취소")
+    }
 }
