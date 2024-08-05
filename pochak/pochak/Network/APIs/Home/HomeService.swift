8f5dac2fe41ad89aff690f7f85cf11e0a5780623
Suyeon <syhhwang1231@gmail.com>
2024-08-05 20:51:54 +0900

    Style: HomeService에 documentation 추가

--- pochak/pochak/Network/APIs/Home/HomeService.swift
+++ pochak/pochak/Network/APIs/Home/HomeService.swift
@@ -8,8 +8,14 @@
 import Foundation
 
 struct HomeService {
-    static func getHomePost(request: HomeRequest, completion: @escaping (_ succeed: HomeResponse?, _ failed: Error?) -> Void) {
-        NetworkService.shared.request(HomeAPI.getHomePost(request)){ response  in
+    /// 홈 탭 게시글 데이터 받아오는 함수
+    /// - Parameters:
+    ///   - request: 받아올 홈 탭 게시글 페이지
+    ///   - completion: 통신 후 핸들러 (뷰컨트롤러에 있음)
+    static func getHomePost(
+        request: HomeRequest,
+        completion: @escaping (_ succeed: HomeResponse?, _ failed: Error?) -> Void) {
+        NetworkService.shared.request(HomeAPI.getHomePost(request)) { response in
             switch response {
             case .success(let data):
                 completion(data, nil)
