72748c89133ebc8679d135f2897ac4bc3819bb55
minjoo <yoogail105@gmail.com>
2024-10-09 21:09:47 +0900

    Feat: 제스처를 통한 줌 기능

--- pochak/pochak/UI/Upload/CameraViewController.swift
+++ pochak/pochak/UI/Upload/CameraViewController.swift
@@ -19,22 +19,38 @@ final class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegat
     
     private var flashMode: AVCaptureDevice.FlashMode = .off
     private let hapticImpact = UIImpactFeedbackGenerator()
+    private var currentZoomFactor: CGFloat = 1.0 {
+        didSet {
+            updateZoomLabel()
+        }
+    }
+    private var lastScale: CGFloat = 1.0
+    private let zoomRate: CGFloat = 0.9
     
     @Published var isCameraBusy = false
     @Published var isFlashOn = false
     
     // MARK: - Views
     
+    @IBOutlet weak var captureBtn: UIButton!
     @IBOutlet weak var previewView: UIView!
     @IBOutlet weak var flashBtnBg: UIButton!
     @IBOutlet weak var flashbtn: UIButton!
+    private var zoomLabel: UILabel = {
+        let label = UILabel()
+        label.text = "1x"
+        label.textColor = .black
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     
     @IBAction func flashBtn(_ sender: Any) {
         switchFlash()
         print("flash")
     }
     
-    @IBAction func captureBtn(_ sender: Any) {
+    @IBAction func capture(_ sender: Any) {
         if isCameraBusy == false {
             hapticImpact.impactOccurred()
             
@@ -56,6 +72,10 @@ final class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegat
         super.viewDidLoad()
         
         self.navigationItem.title = "포착하기"
+        
+        handlePinchGestureForZoom()
+        
+        setupZoomLabel()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -66,6 +86,26 @@ final class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegat
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         self.captureSession.stopRunning()
+    }
+    
+    // MARK: - UI
+    private func setupZoomLabel() {
+        view.addSubview(zoomLabel)
+        
+        NSLayoutConstraint.activate([
+            zoomLabel.centerYAnchor.constraint(equalTo: captureBtn.centerYAnchor),
+            zoomLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 70),
+            zoomLabel.widthAnchor.constraint(equalToConstant: 60),
+            zoomLabel.heightAnchor.constraint(equalToConstant: 30)
+        ])
+        
+        updateZoomLabel()
+    }
+    
+    private func updateZoomLabel() {
+        DispatchQueue.main.async {
+            self.zoomLabel.text = String(format: "%.1fx", self.currentZoomFactor)
+        }
     }
     
     // MARK: - Functions
@@ -131,11 +171,10 @@ final class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegat
                     self.captureSession.addInput(input)
                     self.captureSession.addOutput(self.stillImageOutput)
                     
-                    // 라이브 프리뷰 설정을 메인 스레드에서 호출
                     DispatchQueue.main.async {
                         self.setupLivePreview()
                     }
-                    // captureSession.startRunning()을 백그라운드 스레드에서 호출
+                    
                     self.captureSession.startRunning()
                 }
             } catch let error {
@@ -154,5 +193,61 @@ final class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegat
         
         // previewView의 서브레이어로 추가
         self.previewView.layer.addSublayer(self.videoPreviewLayer)
+    }
+    
+    private func handlePinchGestureForZoom() {
+        let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(handlePinchToZoom(_:)))
+        view.addGestureRecognizer(pinchGesture)
+    }
+    
+    private func setupZoom(for device: AVCaptureDevice) {
+        do {
+            try device.lockForConfiguration()
+            device.videoZoomFactor = 1.0
+            device.unlockForConfiguration()
+        } catch {
+            print("Error setting zoom: \(error.localizedDescription)")
+        }
+    }
+    
+    @objc func handlePinchToZoom(_ gesture: UIPinchGestureRecognizer) {
+        guard let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) else { return }
+        
+        func minMaxZoom(_ factor: CGFloat) -> CGFloat {
+            return min(max(factor, 1.0), 6.0)
+        }
+        
+        func update(scale factor: CGFloat) {
+            do {
+                try device.lockForConfiguration()
+                defer { device.unlockForConfiguration() }
+                device.videoZoomFactor = factor
+                self.currentZoomFactor = factor
+            } catch {
+                print("Error setting zoom: \(error.localizedDescription)")
+            }
+        }
+        
+        var newScaleFactor = minMaxZoom(gesture.scale * currentZoomFactor)
+        
+        switch gesture.state {
+        case .began:
+            lastScale = currentZoomFactor
+        case .changed:
+            let delta = gesture.scale - 1.0
+            if gesture.scale < 1.0 {
+                let fastZoomRate: CGFloat = 2
+                let newScaleFactor = minMaxZoom(lastScale + (delta * zoomRate * fastZoomRate))
+                update(scale: newScaleFactor)
+            } else {
+                let newScaleFactor = minMaxZoom(lastScale + (delta * zoomRate))
+                update(scale: newScaleFactor)
+            }
+        case .ended:
+            newScaleFactor = currentZoomFactor
+            lastScale = newScaleFactor
+        default:
+            break
+        }
     }
 }
