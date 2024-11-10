973be322f7ef3b5412b50afb88231a897866fee5
Suyeon <syhhwang1231@gmail.com>
2024-11-10 17:31:36 +0900

    Feat: '다음' 버튼 커스텀 구현

---
+++ pochak/pochak/UI/Login/Views/NextButton.swift
@@ -0,0 +1,61 @@
+//
+//  NextButton.swift
+//  pochak
+//
+//  Created by Suyeon Hwang on 11/10/24.
+//
+
+import UIKit
+
+final class NextButton: UIButton {
+    
+    // MARK: - Properties
+    
+    var isActive: Bool = false {
+        didSet {
+            setNeedsUpdateConfiguration()
+        }
+    }
+    
+    // MARK: - Initialization
+    
+    init(_ title: String = "다음", _ font: UIFont = UIFont(name: "Pretendard-Bold", size: 16)!) {
+        super.init(frame: .zero)
+        
+        var config = UIButton.Configuration.filled()
+        config.contentInsets = .init(top: 13, leading: 0, bottom: 13, trailing: 0)
+        config.attributedTitle = AttributedString(title)
+        config.attributedTitle?.setAttributes(AttributeContainer([NSAttributedString.Key.font: font]))
+        self.configuration = config
+        
+        let buttonStateHandler: UIButton.ConfigurationUpdateHandler = { button in
+            switch self.isActive {
+            case true:
+                self.isEnabled = true
+                button.configuration?.background.backgroundColor = UIColor(named: "yellow00")
+                //button.configuration?.baseForegroundColor = .white
+                button.configuration?.attributedTitle?.setAttributes(AttributeContainer([NSAttributedString.Key.foregroundColor: UIColor.white, NSAttributedString.Key.font: font]))
+            case false:
+                self.isEnabled = false
+                button.configuration?.background.backgroundColor = UIColor(named: "gray03")
+                //button.configuration?.baseForegroundColor = UIColor(hexCode: "D2C2FF")
+                button.configuration?.attributedTitle?.setAttributes(AttributeContainer([NSAttributedString.Key.foregroundColor: UIColor.white, NSAttributedString.Key.font: font]))
+            }
+        }
+        
+        self.configurationUpdateHandler = buttonStateHandler
+        setupButton()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Functions
+    
+    private func setupButton() {
+        self.clipsToBounds = true
+        self.layer.cornerRadius = 10
+        self.isEnabled = false  // 초기 상태
+    }
+}
