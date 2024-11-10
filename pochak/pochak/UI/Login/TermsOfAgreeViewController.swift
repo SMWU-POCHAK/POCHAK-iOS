c94c7b907a286155709c644c3fcfa7fdb31ee3c5
Suyeon <syhhwang1231@gmail.com>
2024-11-10 18:02:09 +0900

    Chore: 이전 코드 삭제

--- pochak/pochak/UI/Login/TermsOfAgreeViewController.swift
+++ pochak/pochak/UI/Login/TermsOfAgreeViewController.swift
@@ -107,18 +107,6 @@ final class TermsOfAgreeViewController: UIViewController, UIViewControllerTransi
         return button
     }()
     
-    @IBOutlet weak var pochakLabel: UILabel!
-    @IBOutlet weak var pochakCorpLabel: UILabel!
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var appIcon: UIImageView!
-    @IBOutlet weak var agreeForPrivacyPolicy: UIButton!
-    @IBOutlet weak var agreeForTermsOfUSe: UIButton!
-    @IBOutlet weak var seePrivacyPolicy: UIButton!
-    @IBOutlet weak var seeTermsOfUse: UIButton!
-    @IBOutlet weak var agreeAndContinueButton: UIButton!
-    
-    @IBOutlet weak var backgroundView: UIView!
-    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -136,40 +124,6 @@ final class TermsOfAgreeViewController: UIViewController, UIViewControllerTransi
         setupPrivacyPolicyAgreeLabel()
         setupPrivacyPolicyAgreeButton()
         setupNextButton()
-                
-//        backgroundView.layer.cornerRadius = 8
-//        appIcon.layer.cornerRadius = 5
-//        agreeAndContinueButton.layer.cornerRadius = 5
-//        seePrivacyPolicy.setUnderline()
-//        seeTermsOfUse.setUnderline()
-//        
-//        pochakLabel.text = "Pochak"
-//        pochakLabel.font = UIFont(name: "Pretendard-SemiBold", size: 20)
-//        pochakLabel.textColor = .black
-//        
-//        pochakCorpLabel.text = "Pochak Corp."
-//        pochakCorpLabel.font = UIFont(name: "Pretendard-Light", size: 16)
-//        pochakCorpLabel.textColor = UIColor(named: "gray04")
-//        
-//        titleLabel.text = "개인정보 및 이용약관 동의 항목"
-//        titleLabel.font = UIFont(name: "Pretendard-SemiBold", size: 18)
-//        titleLabel.textColor = .black
-//        
-//        
-//        agreeForPrivacyPolicy.setTitle(" [필수] 개인정보 제3자 제공 동의", for: .normal)
-//        agreeForPrivacyPolicy.setImage(UIImage(systemName: "checkmark.circle"), for: .normal)
-//        agreeForPrivacyPolicy.titleLabel?.font =  UIFont(name: "Pretendard-Medium", size: 15)
-//        agreeForPrivacyPolicy.setTitleColor(UIColor(named: "gray04"), for: .normal)
-//        agreeForPrivacyPolicy.tintColor = UIColor(named: "gray04")
-//
-//        
-//        agreeForTermsOfUSe.setTitle(" [필수] 이용약관 항목", for: .normal)
-//        agreeForTermsOfUSe.setImage(UIImage(systemName: "checkmark.circle"), for: .normal)
-//        agreeForTermsOfUSe.titleLabel?.font =  UIFont(name: "Pretendard-Medium", size: 15)
-//        agreeForTermsOfUSe.setTitleColor(UIColor(named: "gray04"), for: .normal)
-//        agreeForTermsOfUSe.tintColor = UIColor(named: "gray04")
-
-        // Do any additional setup after loading the view.
     }
     
     // MARK: - Actions
@@ -181,40 +135,11 @@ final class TermsOfAgreeViewController: UIViewController, UIViewControllerTransi
         checkEachAgreeStatus()
     }
     
-    @IBAction func pressAgreeForPrivacyPolicy(_ sender: Any) {
-        if !didAgreeForPrivacyPolicy {
-            didAgreeForPrivacyPolicy = true
-            agreeForPrivacyPolicy.setTitleColor(UIColor.black, for: .normal)
-            agreeForPrivacyPolicy.setImage(UIImage(systemName: "checkmark.circle.fill"), for: .normal)
-            agreeForPrivacyPolicy.tintColor = UIColor.black
-        } else if didAgreeForPrivacyPolicy {
-            didAgreeForPrivacyPolicy = false
-            agreeForPrivacyPolicy.setTitleColor(UIColor(named: "gray04"), for: .normal)
-            agreeForPrivacyPolicy.setImage(UIImage(systemName: "checkmark.circle"), for: .normal)
-            agreeForPrivacyPolicy.tintColor = UIColor(named: "gray04")
-        }
-        
-    }
-    
     @objc private func privacyPolicyAgreeButtonDidTap() {
         print("개인정보 동의 선택됨")
         privacyPolicyAgreeButton.isChecked.toggle()
         didAgreeForPrivacyPolicy = privacyPolicyAgreeButton.isChecked
         checkEachAgreeStatus()
-    }
-    
-    @IBAction func pressAgreeForTermsOfUSe(_ sender: Any) {
-        if !didAgreeForTermsOfUse {
-            didAgreeForTermsOfUse = true
-            agreeForTermsOfUSe.setTitleColor(UIColor.black, for: .normal)
-            agreeForTermsOfUSe.setImage(UIImage(systemName: "checkmark.circle.fill"), for: .normal)
-            agreeForTermsOfUSe.tintColor = UIColor.black
-        } else if didAgreeForTermsOfUse {
-            didAgreeForTermsOfUse = false
-            agreeForTermsOfUSe.setTitleColor(UIColor(named: "gray04"), for: .normal)
-            agreeForTermsOfUSe.setImage(UIImage(systemName: "checkmark.circle"), for: .normal)
-            agreeForTermsOfUSe.tintColor = UIColor(named: "gray04")
-        }
     }
     
     @objc private func termsOfUseLabelDidTap(_ sender: UITapGestureRecognizer) {
@@ -241,15 +166,6 @@ final class TermsOfAgreeViewController: UIViewController, UIViewControllerTransi
             safariVC.transitioningDelegate = self
             safariVC.modalPresentationStyle = .pageSheet
             present(safariVC, animated: true, completion: nil)
-        }
-    }
-    
-    @IBAction func agreeAndContinue(_ sender: Any) {
-        if didAgreeForPrivacyPolicy && didAgreeForTermsOfUse {
-            self.dismiss(animated: true)
-            delegate?.sendAgreed(agree: true)
-        } else {
-            print("not agreed yet")
         }
     }
     
