995f395f67b7d9e8115729c381bf6e3eb73f2988
minjoo <yoogail105@gmail.com>
2024-12-26 01:32:37 +0900

    Feat: 추억페이지 갤러리뷰 ImageCell

---
+++ pochak/pochak/UI/Memory/Gallery/View/ImageCell.swift
@@ -0,0 +1,65 @@
+//
+//  ImageCell.swift
+//  pochak
+//
+//  Created by Haru on 12/26/24.
+//
+
+import UIKit
+import SnapKit
+
+class ImageCell: UICollectionViewCell {
+    static let identifier = "ImageCell"
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .yellow
+        return imageView
+    }()
+    
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "Pretendard-Bold", size: 12)
+        label.textColor = .white
+        
+        label.layer.shadowColor = UIColor.black.cgColor
+        label.layer.shadowOffset = CGSize(width: 2, height: 2)
+        label.layer.shadowOpacity = 0.8
+        label.layer.shadowRadius = 3
+        
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+        imageView.addSubview(dateLabel)
+        
+        imageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        dateLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(4)
+            $0.leading.equalToSuperview().offset(8)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(with post: MemoryPost) {
+        if let date = post.date {
+            self.dateLabel.text =  Date.extractDay(from: date)
+        }
+                
+        if let url = URL(string: post.imageURL) {
+            imageView.load(with: url)
+        } else {
+            print("Error: URL이 nil입니다.:\(post.imageURL)")
+        }
+    }
+}
