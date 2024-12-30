c445ee03914009335993783c6c18a16e47edec26
minjoo <yoogail105@gmail.com>
2024-12-30 16:46:07 +0900

    chore: 폰트관련 함수 UILabel에서 UITextView로 분리

--- pochak/pochak/Global/Extensions/UILabel+.swift
+++ pochak/pochak/Global/Extensions/UILabel+.swift
@@ -61,9 +61,7 @@ extension UILabel {
             in: textContainer
         )
     }
-}
-
-extension UILabel {
+    
     func applyPochakFont(_ config: PochakFontConfig) {
         self.font = config.font
         let paragraphStyle = NSMutableParagraphStyle()
@@ -84,29 +82,5 @@ extension UILabel {
                                           left: self.layoutMargins.left,
                                           bottom: padding,
                                           right: self.layoutMargins.right)
-    }
-}
-
-extension UITextView {
-    func applyPochakFont(_ config: PochakFontConfig) {
-        self.font = config.font
-        let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.lineSpacing = config.lineHeight - config.font.lineHeight
-        paragraphStyle.alignment = self.textAlignment
-        
-        let attributes: [NSAttributedString.Key: Any] = [
-            .font: config.font,
-            .paragraphStyle: paragraphStyle
-        ]
-        
-        if let text = self.text {
-            self.attributedText = NSAttributedString(string: text, attributes: attributes)
-        }
-        
-        let padding = (config.lineHeight - config.font.lineHeight) / 2
-        self.textContainerInset = UIEdgeInsets(top: padding,
-                                               left: self.textContainerInset.left,
-                                               bottom: padding,
-                                               right: self.textContainerInset.right)
     }
 }
