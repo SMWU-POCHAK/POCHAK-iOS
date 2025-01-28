5770f58def77c88e1cf03b2c84350a0573c85c27
Suyeon <syhhwang1231@gmail.com>
2025-01-29 00:00:15 +0900

    Fix: Central, PeripheralManager의 state를 고려해서 메소드 사용

--- pochak/pochak/Global/Helper/BluetoothSerialManager.swift
+++ pochak/pochak/Global/Helper/BluetoothSerialManager.swift
@@ -47,7 +47,7 @@ final class BluetoothSerialManager: NSObject {
         super.init()
         self.centralManager = CBCentralManager(delegate: self, queue: nil)
         self.peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
-        peripheralManager.add(CBMutableService(type: serviceUUID, primary: true))
+//        peripheralManager.add(CBMutableService(type: serviceUUID, primary: true))
         self.currentMode = .scanningMode
     }
     
@@ -56,7 +56,9 @@ final class BluetoothSerialManager: NSObject {
     func setBluetoothModeAndStart(to mode: BluetoothMode) {
         switch mode {
         case .advertisingMode:
-            centralManager.stopScan()
+            if centralManager.isScanning {  // .poweredOn 일때만 해야함
+                centralManager.stopScan()
+            }
             currentMode = .advertisingMode
             self.startAdvertising()
         case .scanningMode:
@@ -69,22 +71,19 @@ final class BluetoothSerialManager: NSObject {
     
     /// 기기 검색 시작, 연결이 가능한 모든 주변기기를 serviceUUID를 통해 검색
     func startScan() {
-        if !centralManager.isScanning {
-            print("=== [BluetoothSerialManager] startScan ===")
-            print(">> state: \(centralManager.state)")  // 5: poweredOn
-            
-            if centralManager.state == .poweredOn {
+        if centralManager.state == .poweredOn {
+            if !centralManager.isScanning {
+                print("[BluetoothSerialManager] startScan ===")
                 // withService가 nil 이면 모든 종류의 기기 검색 / 입력하면 특정 serviceUUID를 가진 기기만 검색 -> 포착은 후자의 경우
                 let options = [CBCentralManagerScanOptionAllowDuplicatesKey: false]  // 이미 스캔된 정보면 다시 스캔 안 하도록 false
                 centralManager.scanForPeripherals(withServices: [serviceUUID], options: options)
             }
             else {
-                print("![Error] Central Manager cannot scan because state is not poweredOn!")
+                print("![Error] Central manager is already scanning!")
             }
-            print("==========================================")
         }
         else {
-            print("![Error] Central manager is already scanning!")
+            print("![Error] Central Manager cannot scan because state is not poweredOn! current state: \(centralManager.state)")
         }
     }
     
@@ -94,6 +93,10 @@ final class BluetoothSerialManager: NSObject {
     
     /// periphalManager에 service를 추가한 후 advertise 시작하는 메소드
     func startAdvertising() {
+        guard peripheralManager.state == .poweredOn else {
+            print("[!] Error: Cannot start advertising, Peripheral manager state is \(peripheralManager.state)")
+            return
+        }
         peripheralManager.removeAllServices()
         peripheralManager.add(CBMutableService(type: serviceUUID, primary: true))
         peripheralManager.startAdvertising([
@@ -125,7 +128,7 @@ extension BluetoothSerialManager: CBCentralManagerDelegate {
             print(">> power Off")
         case .poweredOn:
             print(">> power on")
-            if !centralManager.isScanning { startScan() }
+            if !centralManager.isScanning && currentMode == .scanningMode { startScan() }
         @unknown default:
             fatalError()
         }
@@ -134,12 +137,11 @@ extension BluetoothSerialManager: CBCentralManagerDelegate {
     // 기기가 검색될 때마다 호출, 여기서 커스텀한 service만 찾을 수 있도록
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
         let localName = advertisementData[CBAdvertisementDataLocalNameKey] as? String
-        print("=== central manager did discover peripheral ===")
+        print("[BluetoothSerialManager] central manager did discover peripheral")
         print(">> name: \(peripheral.name ?? "UNKNOWN NAME")")
         print(">> uuid: \(peripheral.identifier.uuidString)")
-        print(">> advertisementData, local name: \(advertisementData[CBAdvertisementDataLocalNameKey])")
+        print(">> advertisementData, local name: \(localName)")
         print(">> advertisementData, service uuid: \(advertisementData[CBAdvertisementDataServiceUUIDsKey])")
-        print("===============================================")
         
         // advertisement data가 제대로 왔을 때에만 delegate 메소드 호출
         if localName != nil {
@@ -152,33 +154,33 @@ extension BluetoothSerialManager: CBCentralManagerDelegate {
 
 extension BluetoothSerialManager: CBPeripheralManagerDelegate {
     func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
-        print("=== peripheralManagerDidUpdateState ===")
+        print("[BluetoothSerialManager] peripheralManagerDidUpdateState:")
         switch peripheral.state {
         case .unknown:
-            print("unknown")
+            print(">> unknown")
         case .resetting:
-            print("restting")
+            print(">> resetting")
         case .unsupported:
-            print("unsupported")
+            print(">> unsupported")
         case .unauthorized:
-            print("unauthorized")
+            print(">> unauthorized")
         case .poweredOff:
-            print("power Off")
+            print(">> power Off")
         case .poweredOn:
-            print("power on")
-            self.peripheralManager.removeAllServices()
-            self.peripheralManager.add(CBMutableService(type: serviceUUID, primary: true))
+            print(">> power on")
+            if currentMode == .advertisingMode {
+                startAdvertising()
+            }
         @unknown default:
             fatalError()
         }
     }
     
     func peripheralManagerDidStartAdvertising(_ peripheral: CBPeripheralManager, error: (any Error)?) {
-        print("=== peripheralManagerDidStartAdvertising ===")
-        print(peripheral.isAdvertising)
+        print("[BluetoothSerialManager] peripheralManagerDidStartAdvertising:")
+        print(">> \(peripheral.isAdvertising)")
         if let error = error {
             print("![Error] \(error.localizedDescription)")
         }
-        print("============================================")
     }
 }
