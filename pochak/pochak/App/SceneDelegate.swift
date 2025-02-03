1b38feec1c80dc58a4e803c5e9eb67718746b970
Suyeon <syhhwang1231@gmail.com>
2025-02-03 21:27:32 +0900

    Fix: 백그라운드 태스크 코드 약간 수정

--- pochak/pochak/App/SceneDelegate.swift
+++ pochak/pochak/App/SceneDelegate.swift
@@ -71,8 +71,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
         
-        // 앱이 Background 상태에 들어갈 때 BGTaskScheduler에 Task를 submit
-        (UIApplication.shared.delegate as? AppDelegate)?.scheduleBackgroundTask()
+        // 앱이 Background 상태에 들어갈 때 BGTaskScheduler에 Task를 submit -> AppDelegate로 옮김
+//        (UIApplication.shared.delegate as? AppDelegate)?.scheduleBackgroundTask()
     }
     
     // Root 화면 전환
