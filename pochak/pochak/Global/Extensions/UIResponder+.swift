f9a1fee51547f55e24f07b0b362e4d5b96ca2d2a
Suyeon <syhhwang1231@gmail.com>
2025-02-10 20:33:30 +0900

    Refactor: 모호한 변수명 수정

--- pochak/pochak/Global/Extensions/UIResponder+.swift
+++ pochak/pochak/Global/Extensions/UIResponder+.swift
@@ -9,22 +9,22 @@ import UIKit
 
 extension UIResponder {
 
-    private struct Static {
+    private struct StaticShared {
         static weak var responder: UIResponder?
     }
 
     /// 현재 응답자 반환하는 computed property
     static var currentResponder: UIResponder? {
         // Static.responder 초기화
-        Static.responder = nil
+        StaticShared.responder = nil
         // 특정 동작을 유발시켜서 현재 응답자를 찾아내는 방식
         UIApplication.shared.sendAction(#selector(UIResponder._trap), to: nil, from: nil, for: nil)
         // 찾아낸 UIResponder 반환
-        return Static.responder
+        return StaticShared.responder
     }
 
     /// 현재 응답자 저장하는 메소드
     @objc private func _trap() {
-        Static.responder = self
+        StaticShared.responder = self
     }
 }
