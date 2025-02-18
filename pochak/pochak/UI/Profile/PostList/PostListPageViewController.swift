9c4a98e030a69f3b845c138a73051cd46137c021
Suyeon <syhhwang1231@gmail.com>
2025-02-18 21:26:03 +0900

    Feat: 프로필 탭 포착/포착드 페이지뷰컨트롤러 구현

---
+++ pochak/pochak/UI/Profile/PostList/PostListPageViewController.swift
@@ -0,0 +1,163 @@
+//
+//  PostListPageViewController.swift
+//  pochak
+//
+//  Created by Suyeon Hwang on 2/16/25.
+//
+
+import UIKit
+import SnapKit
+
+enum PostListType {
+    case POCHAKED
+    case POCHAK
+}
+
+final class PostListPageViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    let type: PostListType
+    let myProfileTabVC: MyProfileTabViewController
+    
+    static private let minimumLineSpacing: CGFloat = 9
+    static private let minimumInterItemSpacing: CGFloat = 8
+    
+    private var postList: [ProfilePostList] = []
+    
+    // MARK: - Views
+    
+    lazy var collectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .vertical
+        flowLayout.minimumLineSpacing = PostListPageViewController.minimumLineSpacing
+        flowLayout.minimumInteritemSpacing = PostListPageViewController.minimumInterItemSpacing
+        
+        let view = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        view.isScrollEnabled = true
+        view.showsHorizontalScrollIndicator = false
+        view.showsVerticalScrollIndicator = false
+        view.alwaysBounceVertical = true
+        view.contentInset = .init(top: 20, left: 20, bottom: 20, right: 20)
+        view.dataSource = self
+        view.delegate = self
+        view.register(PostListCollectionViewCell.self, forCellWithReuseIdentifier: PostListCollectionViewCell.identifier)
+        return view
+    }()
+    
+    // MARK: - Init
+    
+    init(type: PostListType, parentVC: MyProfileTabViewController) {
+        self.type = type
+        self.myProfileTabVC = parentVC
+
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addViews()
+        setupConstraints()
+    }
+    
+//    override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+//        
+//        self.collectionView.layoutIfNeeded()
+//        self.view.layoutIfNeeded()
+//        self.myProfileTabVC.scrollView.updateContentSize()
+//    }
+    
+    // MARK: - Layout
+    
+    private func addViews() {
+        view.addSubview(collectionView)
+        view.backgroundColor = UIColor(named: "gray02")
+    }
+    
+    private func setupConstraints() {
+        collectionView.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.top.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+    }
+    
+    // MARK: - Functions
+    
+    func setPostCollectionViewData(_ postList: [ProfilePostList]) {
+        self.postList = postList
+        self.collectionView.reloadData()
+        
+        print("✅ collectionView frame: \(collectionView.frame)")
+        self.myProfileTabVC.scrollView.updateContentSize()
+    }
+    
+//    func updateCollectionViewHeight() {
+//        collectionView.snp.updateConstraints { make in
+//            make.height.equalTo(collectionView.collectionViewLayout.collectionViewContentSize.height)
+//        }
+//        collectionView.layoutIfNeeded()
+//    }
+}
+
+// MARK: - Extension; UICollectionView
+
+extension PostListPageViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return noPost ? 1 : postList.count
+        print("[PostListPageViewController] collectionview - item 개수")
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        print("[PostListPageViewController] collectionview - cell 설정 -- ")
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PostListCollectionViewCell.identifier, for: indexPath) as? PostListCollectionViewCell else { return UICollectionViewCell() }
+        //cell.configure(with: <#T##String#>)
+        
+        if indexPath.item == 19 {
+            myProfileTabVC.updateScrollViewContentSize()
+        }
+        print("[PostListPageViewController] collectionview - cell 설정")
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        if !noPost {
+//            let exploreTabSb = UIStoryboard(name: "ExploreTab", bundle: nil)
+//            guard let postVC = exploreTabSb.instantiateViewController(withIdentifier: "PostVC") as? PostViewController
+//            else { return }
+//            
+//            postVC.receivedPostId = postList[indexPath.item].postId
+//            self.navigationController?.pushViewController(postVC, animated: true)
+//        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        myProfileTabVC.updateScrollViewContentSize()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout: UICollectionViewLayout, minimumLineSpacingForSectionAt: Int) -> CGFloat {
+        return PostListPageViewController.minimumLineSpacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return PostListPageViewController.minimumInterItemSpacing
+    }
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+//        return .init(top: 20, left: 20, bottom: 20, right: 20)
+//    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = CGFloat((collectionView.frame.width - 20 * 2 - PostListPageViewController.minimumInterItemSpacing * 2) / 3)  // 20은 양 끝 간격(inset)
+        return CGSize(width: width, height: width * 4 / 3)  // 3:4 비율로
+    }
+}
