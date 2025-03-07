16873e4a90005a7169edd2f98b19e8fcc61fbcaa
Suyeon <syhhwang1231@gmail.com>
2025-03-08 01:34:47 +0900

    Fix: 프로필탭 - 첫번째 페이지(포착드) 데이터가 없을 경우 레이아웃 깨짐 문제 해결

--- pochak/pochak/UI/Profile/MyProfileTabViewController.swift
+++ pochak/pochak/UI/Profile/MyProfileTabViewController.swift
@@ -620,12 +620,13 @@ final class MyProfileTabViewController: UIViewController {
     }
     
     func updateScrollViewContentSize() {
-        print("[MyProfileTabViewController] updateScrollViewContentSize =============")
+        print("[OtherUserProfileViewController] updateScrollViewContentSize =============")
         print(">> currentPage: \(currentPage)")
         let vc = self.pageViewControllerList[currentPage] as? PostListPageViewController
-        let newHeight = (vc?.collectionView.collectionViewLayout.collectionViewContentSize.height)! + 20 * 2 + profileView.frame.height + segmentContainerView.frame.height
+        let newHeight = contentView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize).height + (vc?.collectionView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize).height ?? 0) + 20 * 2
         scrollView.contentSize = CGSize(width: scrollView.frame.width, height: newHeight)
         contentView.frame = CGRect(x: 0, y: 0, width: scrollView.frame.width, height: newHeight)
+        print(">> Updated scrollView.contentSize: \(scrollView.contentSize)")
     }
 }
 
