631b9debf57a7f496063b5ef4186a80a3f52e480
Suyeon <syhhwang1231@gmail.com>
2025-03-28 19:06:21 +0900

    Refactor: appleID static 변수로 지정

--- pochak/pochak/Global/Helper/AppStoreUpdateManager.swift
+++ pochak/pochak/Global/Helper/AppStoreUpdateManager.swift
@@ -15,13 +15,13 @@ final class AppStoreUpdateManager {
     /// 현재 기기에 설치된 앱의 버전
     static let currentAppVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
     static let buildNumber = Bundle.main.infoDictionary?["CFBundleVersion"] as? String
+    static let appleID = "6502332418"
     static let appStoreOpenUrlString = "itms-apps://itunes.apple.com/app/apple-store/6502332418"
     
     /// 현재 앱스토어에 올라가있는 최신 버전 정보을 확인하는 메소드입니다.
     /// - Returns: 최신 버전
     func getLatestVersion() -> String? {
-        let appleID = "6502332418"
-        guard let url = URL(string: "https://itunes.apple.com/lookup?id=\(appleID)&country=kr"),
+        guard let url = URL(string: "https://itunes.apple.com/lookup?id=\(AppStoreUpdateManager.appleID)&country=kr"),
               let data = try? Data(contentsOf: url),
               let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any],
               let results = json["results"] as? [[String: Any]],
