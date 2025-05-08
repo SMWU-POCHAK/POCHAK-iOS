039121b6d4c508c2e6820acfab3854335c8126b2
Suyeon <syhhwang1231@gmail.com>
2025-05-08 19:11:31 +0900

    Refactor: FollowButton init 안의 코드 모두 함수화

--- pochak/pochak/UI/Profile/FollowButton.swift
+++ pochak/pochak/UI/Profile/FollowButton.swift
@@ -22,18 +22,34 @@ final class FollowButton: UIButton {
     init() {
         super.init(frame: .zero)
         
-        let font = UIFont(name: "Pretendard-Bold", size: 16)
+        let font = UIFont(name: "Pretendard-Bold", size: 16) ?? UIFont.systemFont(ofSize: 16, weight: .bold)
         
+        self.configuration = self.configureInitialButtonState(font: font)  // button configuration
+        self.configurationUpdateHandler = self.createButtonStateHandler(font: font)  // button state handler
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Function
+    
+    /// 팔로우버튼의 configuration 초기화
+    /// - Parameter font: 팔로우 버튼에 사용할 폰트
+    /// - Returns: UIButton.Configuation
+    private func configureInitialButtonState(font: UIFont) -> UIButton.Configuration {
         var config = UIButton.Configuration.filled()
         config.attributedTitle = AttributedString("팔로우")
-        config.attributedTitle?.setAttributes(AttributeContainer([NSAttributedString.Key.font : font,
+        config.attributedTitle?.setAttributes(AttributeContainer([NSAttributedString.Key.font: font,
                                                                   NSAttributedString.Key.foregroundColor: UIColor.white]))
         config.background.cornerRadius = 15
         config.contentInsets = .init(top: 10, leading: 0, bottom: 10, trailing: 0)
         config.baseBackgroundColor = UIColor(named: "yellow00")
         
-        self.configuration = config
-        
+        return config
+    }
+    
+    private func createButtonStateHandler(font: UIFont) -> UIButton.ConfigurationUpdateHandler {
         let buttonStateHandler: UIButton.ConfigurationUpdateHandler = { button in
             switch self.isFollowing {
             case true:
@@ -46,11 +62,6 @@ final class FollowButton: UIButton {
                 button.configuration?.attributedTitle?.setAttributes(AttributeContainer([NSAttributedString.Key.foregroundColor: UIColor.white, NSAttributedString.Key.font: font]))
             }
         }
-        
-        self.configurationUpdateHandler = buttonStateHandler
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        return buttonStateHandler
     }
 }
