e9d1f1d5cb5ee990703ac8b3a48662f049576ea8
Suyeon <syhhwang1231@gmail.com>
2025-05-08 21:30:09 +0900

    Chore: 불필요한 주석문 삭제

--- pochak/pochak/App/SceneDelegate.swift
+++ pochak/pochak/App/SceneDelegate.swift
@@ -65,8 +65,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
-        print("=== scene will enter foreground ===")
-        //self.checkAndUpdateIfNeeded()
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
