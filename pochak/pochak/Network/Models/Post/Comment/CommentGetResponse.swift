12e293082625ea194f9a497b5bf97f9547dba652
Suyeon <syhhwang1231@gmail.com>
2025-06-27 22:21:07 +0900

    Feat: CommentModel 추가 및 Response를 model로 바꾸는 static function 추가

--- pochak/pochak/Network/Models/Post/Comment/CommentGetResponse.swift
+++ pochak/pochak/Network/Models/Post/Comment/CommentGetResponse.swift
@@ -49,3 +49,36 @@ struct ChildCommentData: Codable {
     let createdDate: String
     let content: String
 }
+
+extension CommentGetResponse {
+    
+    static func convertCommentGetResponseToModel(_ DTO: CommentGetResponse) -> CommentModel {
+        // 현재 게시물의 전체 댓글의 페이지에 대한 정보
+        let commentPageModel = PageModel(isLastPage: DTO.result.parentCommentPageInfo.lastPage,
+                                         isFetchingFirstPage: true,
+                                         currentFetchingPage: 0)
+        let commentDataModelList = DTO.result.parentCommentList.map {
+            CommentDataModel(commentId: $0.commentId,
+                             profileImage: $0.profileImage,
+                             handle: $0.handle,
+                             createdDate: $0.createdDate,
+                             content: $0.content,
+                             childCommentPageModel: PageModel(isLastPage: $0.childCommentPageInfo.lastPage,
+                                                              isFetchingFirstPage: true,
+                                                              currentFetchingPage: 0),
+                             childCommentCnt: $0.childCommentList.count,
+                             childCommentModelList: $0.childCommentList.map { child in
+                                ChildCommentModel(commentId: child.commentId,
+                                                  profileImage: child.profileImage,
+                                                  handle: child.handle,
+                                                  createdDate: child.createdDate,
+                                                  content: child.content)
+                            }
+            )
+        }
+
+        return CommentModel(loginMemberProfileiimage: DTO.result.loginMemberProfileImage,
+                            commentDataModelList: commentDataModelList,
+                            commentPageModel: commentPageModel)
+    }
+}
