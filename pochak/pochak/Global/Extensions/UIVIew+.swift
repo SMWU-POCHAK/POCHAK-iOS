0c7a0cee76f0cf8fb9ffebe3bd4624b338af5a23
Suyeon <syhhwang1231@gmail.com>
2025-08-11 21:14:33 +0900

    Feat: 그래디언트를 설정하는 UIView extension 메소드

--- pochak/pochak/Global/Extensions/UIVIew+.swift
+++ pochak/pochak/Global/Extensions/UIVIew+.swift
@@ -14,4 +14,20 @@ extension UIView {
         mask.path = path.cgPath
         self.layer.mask = mask
     }
+    
+    func setGradient(color1: UIColor, color2: UIColor, isHorizontal: Bool){
+        let gradient: CAGradientLayer = CAGradientLayer()
+        gradient.colors = [color1.cgColor,color2.cgColor]
+        gradient.locations = [0.0, 1.0]
+        if isHorizontal {
+            gradient.startPoint = CGPoint(x: 0.0, y: 1.0)
+            gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+        }
+//        else {
+//            gradient.startPoint = CGPoint(x: 0.0, y: 1.0)
+//            gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+//        }
+        gradient.frame = bounds
+        layer.addSublayer(gradient)
+    }
 }
